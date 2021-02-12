dbd8c24688a52408bc3dad33488cd27f22276fea
egoroff <egoroff@gmail.com>
2021-02-12 16:27:47 +0300

    single thread implementation using std::fs. Hidden files scanning enabled for parallel

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,30 @@
 use std::time::Instant;
-use walkdir::WalkDir;
+use std::fs;
+use std::collections::VecDeque;
 
 extern crate humantime;
 extern crate jwalk;
-extern crate walkdir;
 
 pub fn execute_single(path: &str) {
     let now = Instant::now();
 
     let mut files: i64 = 0;
     let mut folders: i64 = 0;
-    let mut total_size: u64 = 0;
-    for entry in WalkDir::new(path).follow_links(false) {
-        if let Ok(e) = entry {
-            if e.file_type().is_file() {
-                if let Ok(m) = e.metadata() {
-                    total_size += m.len()
-                }
-                files += 1;
-            } else if e.file_type().is_dir() {
-                folders += 1;
-            }
+    let total_size: u64 = 0;
+
+    let mut vector: VecDeque<String> = VecDeque::new();
+    vector.push_back(String::from(path));
+
+    loop {
+        if vector.is_empty() {
+           break;
+        } else {
+            let p = vector.pop_front().unwrap();
+            folders += 1;
+            read_dir(&mut vector, &p, &mut files);
         }
     }
+
     println!(
         "files: {} folders: {} size: {} elapsed: {}",
         files,
@@ -32,13 +34,27 @@ pub fn execute_single(path: &str) {
     );
 }
 
+fn read_dir(vector: &mut VecDeque<String>, path: &str, f: &mut i64) {
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let full = entry.path().into_os_string().into_string().unwrap();
+        if entry.path().is_dir() {
+            //visit_dirs(&path, cb).;
+            vector.push_back(full);
+        } else {
+            //println!("{}", full);
+            *f += 1;
+        }
+    }
+}
+
 pub fn execute_parallel(path: &str) {
     let now = Instant::now();
 
     let mut files: i64 = 0;
     let mut folders: i64 = 0;
     let mut total_size: u64 = 0;
-    for entry in jwalk::WalkDir::new(path).follow_links(false) {
+    for entry in jwalk::WalkDir::new(path).skip_hidden(false).follow_links(false) {
         if let Ok(e) = entry {
             if e.file_type().is_file() {
                 if let Ok(m) = e.metadata() {
